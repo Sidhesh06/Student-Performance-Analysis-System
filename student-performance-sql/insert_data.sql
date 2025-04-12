38cb0b0328b194031a26aacc81cb7721a2da9469
Sidhesh Wandhare <140540597+Sidhesh06@users.noreply.github.com>
2025-04-12 17:03:29 +0530

    Create insert_data.sql

---
+++ student-performance-sql/insert_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO Students VALUES
+(1, 'Vedashree Wandhare', 'F', 7, 'C'),
+(2, 'Ankit Sharma', 'M', 7, 'C'),
+(3, 'Riya Patel', 'F', 7, 'B');
+
+INSERT INTO Subjects VALUES
+(101, 'Mathematics'),
+(102, 'Science'),
+(103, 'English');
+
+INSERT INTO Marks VALUES
+(1, 1, 101, 78),
+(2, 1, 102, 85),
+(3, 1, 103, 67),
+(4, 2, 101, 66),
+(5, 2, 102, 72),
+(6, 2, 103, 81),
+(7, 3, 101, 88),
+(8, 3, 102, 79),
+(9, 3, 103, 90);
